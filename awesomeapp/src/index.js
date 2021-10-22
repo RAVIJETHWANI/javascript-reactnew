8766b5362ce4c52f62ef21f6c65003bdfe55392d
ravi <rjsam46@gmail.com>
2021-10-22 11:40:49 +0530

    my first commit reactwebsite

--- awesomeapp/src/index.js
+++ awesomeapp/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from "react-router-dom"
-import MovieApp from './MovieApp';
-import { AppProvider } from './MovieContext';
-import "./Movie.css"
+import HackerNewsApp from './HackerNewsApp';
+import { AppProvider } from './HackerNewsContext';
+import "./HackerNews.css"
 
 //import store from "./Store"
 //import {Provider} from "react-redux"
@@ -12,7 +12,7 @@ ReactDOM.render(
 <>
 <BrowserRouter>
 <AppProvider>
-  <MovieApp />
+  <HackerNewsApp />
 </AppProvider>
   
 </BrowserRouter>
